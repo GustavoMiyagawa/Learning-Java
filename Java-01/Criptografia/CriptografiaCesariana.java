0249a792f4bdb2363a491a70e52bf114e9b36f4f
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-12-23 15:39:55 -0400

    Adicionado projeto Criptografia Julio Cesar em Java.

---
+++ Java-01/Criptografia/CriptografiaCesariana.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+public class CriptografiaCesariana implements Criptografia {
+
+    @Override
+    public String criptografar(String texto) {
+    	if (texto == "") throw new IllegalArgumentException();
+    
+        String lowerCase = texto.toLowerCase();
+
+        ArrayList<Character> encryptList = new ArrayList();
+
+        for (int i=0; i < lowerCase.length(); i++) {
+            if ((int)lowerCase.charAt(i) == 32) {
+                encryptList.add(lowerCase.charAt(i));
+            } else if ((int)lowerCase.charAt(i)+3 == 123) {
+                encryptList.add((char)97);
+            } else if ((int)lowerCase.charAt(i)+3 == 124){
+                encryptList.add((char)98);
+            } else if ((int)lowerCase.charAt(i)+3 == 125) {
+                encryptList.add((char)99);
+            } else if ((int)lowerCase.charAt(i) >= 48 && (int)lowerCase.charAt(i) <= 57) {
+                encryptList.add((char)((int)lowerCase.charAt(i)));
+            } else {
+                encryptList.add((char)((int)lowerCase.charAt(i)+3));
+            }
+        }
+
+        String encryptText = "";
+
+        for (Character c : encryptList) {
+            encryptText += String.valueOf(c);
+        }
+        return encryptText;
+    }
+
+    @Override
+    public String descriptografar(String texto) {
+        if (texto == "") throw new IllegalArgumentException();
+    
+        String lowerCase = texto.toLowerCase();
+
+        ArrayList<Character> decryptList = new ArrayList();
+
+        for (int i=0; i < lowerCase.length(); i++) {
+            if ((int)lowerCase.charAt(i) == 32) {
+                decryptList.add(lowerCase.charAt(i));
+            } else if ((int)lowerCase.charAt(i)-3 == 94) {
+                decryptList.add((char)120);
+            } else if ((int)lowerCase.charAt(i)-3 == 95){
+                decryptList.add((char)121);
+            } else if ((int)lowerCase.charAt(i)-3 == 96) {
+                decryptList.add((char)122);
+            } else if ((int)lowerCase.charAt(i) >= 48 && (int)lowerCase.charAt(i) <= 57) {
+                decryptList.add((char)((int)lowerCase.charAt(i)));
+            } else {
+                decryptList.add((char)((int)lowerCase.charAt(i)-3));
+            }
+        }
+
+        String decryptText = "";
+
+        for (Character c : decryptList) {
+            decryptText += String.valueOf(c);
+        }
+        return decryptText;
+    }
+}
---
+++ Java-01/Criptografia/README.md
@@ -0,0 +1,13 @@
+# Criptografia de Júlio César
+
+Neste projeto implementei a criptografia e descriptografia utilizada pelo grande líder militar romano Júlio César. Essa criptografia se baseava na substituição da letra do alfabeto avançado três casas. Por exemplo:
+
+* **Normal**:  `Texto a ser criptografado`
+* **Cifrado**: `whawr d vhu fulswrjudidgr`
+
+Detalhes da implementação:
+
+* As mensagens serão convertidas para minúsculas tanto para a criptografia quanto para descriptografia.
+* No nosso caso os números serão mantidos, ou seja:
+    * Normal: 1
+    * Cifrado: 1
